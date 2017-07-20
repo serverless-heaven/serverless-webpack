c42f8428ff9992fe573018743f0484356af3994a
Francisco Guimarães <francisco.cpg@gmail.com>
2017-07-20 11:46:10 -0300

    - Adding babel-multiple-statically-entries example - Renaming multiple-entries-example to multiple-statically-entries

---
+++ examples/babel-multiple-statically-entries/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    first: ['./first.js'],
+    second: ['./second.js']
+  },
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  }
+};
