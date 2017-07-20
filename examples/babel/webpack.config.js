f1f5f223e4ca5d3c6dfa2aa5e70e7a735b9b4623
Francisco Guimarães <francisco.cpg@gmail.com>
2017-07-20 10:07:56 -0300

    Fix babel example

--- examples/babel/webpack.config.js
+++ examples/babel/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
+      loaders: ['babel-loader'],
       include: __dirname,
       exclude: /node_modules/,
     }]
