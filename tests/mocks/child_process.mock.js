269609e7316afb3f55eccb7aa47a52e01c7d7ac3
Frank Schmid <fschmid@stashimi.com>
2017-08-09 18:12:59 +0200

    Support transient dependencies when bundling (whitelisting) node modules

--- tests/mocks/child_process.mock.js
+++ tests/mocks/child_process.mock.js
@@ -7,6 +7,7 @@
 module.exports.create = sandbox => {
   const childProcessMock = {
     exec: sandbox.stub().yields(),
+    execSync: sandbox.stub().returns('{}')
   };
 
   return childProcessMock;
