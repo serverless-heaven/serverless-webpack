39b962807d378df7d6526f87e101b63601fda5f4
Francisco Guimarães <francisco.cpg@gmail.com>
2017-09-18 15:21:01 -0300

    - Upgrade deps (#227)
    * - Upgrade deps
    - Improve webpack.config to import ts files without adding extension
    
    * Using default extensions plus ts extensions
    
    * Using webpack v3 defaults

--- examples/typescript/webpack.config.js
+++ examples/typescript/webpack.config.js
@@ -1,8 +1,16 @@
-var path = require('path');
+const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.ts',
+      '.tsx'
+    ]
+  },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
