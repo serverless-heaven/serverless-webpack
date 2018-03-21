ce70b5303dc181f56c5bf1e6c1f85a7e2829760c
Frank Schmid <fschmid@stashimi.com>
2018-03-21 17:51:11 +0100

    Added Webpack 4 example

---
+++ examples/babel-webpack-4/README.md
@@ -0,0 +1,6 @@
+This is the default babel webpack-4 example to look at, because only dynamic entry point resolution lets you use Serverless completely. Individual packaging with a per-function dependency optimization is only available with that approach.
+
+You can also try to invoke a function locally:
+```
+serverless invoke local --function=first --path=./event.json
+```
---
+++ examples/babel-webpack-4/lib/App.js
@@ -0,0 +1,25 @@
+import BbPromise from 'bluebird';
+
+const THE_MESSAGE = 'Hello from the webpack 4 sample';
+
+export class App {
+  static handleFirst(event) {
+    const myDemoResult = {
+      message: App.THE_MESSAGE,
+      from: 'First lambda ;-)',
+      event
+    };
+    
+    return BbPromise.resolve(myDemoResult);
+  }
+
+  static handleSecond(event) {
+    const myDemoResult = {
+      message: THE_MESSAGE,
+      from: 'Second lambda ;-)',
+      event
+    };
+    
+    return BbPromise.resolve(myDemoResult);
+  }
+}
