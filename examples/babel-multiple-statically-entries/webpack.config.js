eec2cb8aec87579f0e2ec116afa28882a6ec0ff3
Steve Mao <maochenyan@gmail.com>
2018-09-27 09:31:17 +1000

    commonjs2 is the one that nodejs is using
    See https://github.com/webpack/webpack/issues/1114

--- examples/babel-multiple-statically-entries/webpack.config.js
+++ examples/babel-multiple-statically-entries/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     ]
   },
   output: {
-    libraryTarget: 'commonjs',
+    libraryTarget: 'commonjs2',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js'
   }
