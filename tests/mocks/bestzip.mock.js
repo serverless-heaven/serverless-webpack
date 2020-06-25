6a78a15a038b0f0a46933695729c19d2d9932ba1
Liron Kopinsky <liron@strattic.com>
2020-06-25 16:52:38 +0300

    Get unit tests to pass and attempt to fix zip file paths.

--- tests/mocks/bestzip.mock.js
+++ tests/mocks/bestzip.mock.js
@@ -6,16 +6,10 @@
 
 const sinon = require('sinon');
 
-// const BestZipMock = sandbox => ({
-//   bestzip: sandbox.stub()
-// });
 const BestZipMock = sandbox => sandbox.stub();
 
 module.exports.create = sandbox => {
   const bestzipMock = BestZipMock(sandbox);
-  const mock = {
-    create: sinon.stub().returns(bestzipMock),
-    _bestzipMock: bestzipMock
-  };
+  const mock = sinon.stub().resolves(bestzipMock);
   return mock;
 };
