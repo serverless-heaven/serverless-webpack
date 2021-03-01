1cf851d94bded664f21c4d0ae256d8130e040e46
Vicary <vicary.archangel@member.mensa.org>
2021-03-01 20:27:44 +0800

    remove rebase artifacts

--- tests/packageModules.test.js
+++ tests/packageModules.test.js
@@ -358,54 +358,6 @@ describe('packageModules', () => {
         module.compileStats = stats;
         return expect(module.packageModules()).to.be.rejectedWith('Packaging: No files found');
       });
-
-      it('should reject if no files are found because all files are excluded using regex', () => {
-        module.configuration = new Configuration({
-          webpack: {
-            excludeRegex: /.*/
-          }
-        });
-
-        // Test data
-        const stats = {
-          stats: [
-            {
-              compilation: {
-                compiler: {
-                  outputPath: '/my/Service/Path/.webpack/service'
-                }
-              }
-            }
-          ]
-        };
-        const files = [ 'README.md', 'src/handler1.js', 'src/handler1.js.map', 'src/handler2.js', 'src/handler2.js.map' ];
-        const allFunctions = [ 'func1', 'func2' ];
-        const func1 = {
-          handler: 'src/handler1',
-          events: []
-        };
-        const func2 = {
-          handler: 'src/handler2',
-          events: []
-        };
-        // Serverless behavior
-        sandbox.stub(serverless.config, 'servicePath').value('/my/Service/Path');
-        getVersionStub.returns('1.18.0');
-        getServiceObjectStub.returns({
-          name: 'test-service'
-        });
-        getAllFunctionsStub.returns(allFunctions);
-        getFunctionStub.withArgs('func1').returns(func1);
-        getFunctionStub.withArgs('func2').returns(func2);
-        // Mock behavior
-        globMock.sync.returns(files);
-        fsMock._streamMock.on.withArgs('open').yields();
-        fsMock._streamMock.on.withArgs('close').yields();
-        fsMock._statMock.isDirectory.returns(false);
-
-        module.compileStats = stats;
-        return expect(module.packageModules()).to.be.rejectedWith('Packaging: No files found');
-      });
     });
 
     describe('with individual packaging', () => {
@@ -532,12 +484,8 @@ describe('packageModules', () => {
             expect(fsMock.copyFileSync).to.be.calledWith(expectedArtifactSource, expectedArtifactDestination),
 
             // Should set package artifact for each function to the single artifact
-            expect(func1)
-              .to.have.a.nested.property('package.artifact')
-              .that.equals(expectedArtifactDestination),
-            expect(func2)
-              .to.have.a.nested.property('package.artifact')
-              .that.equals(expectedArtifactDestination)
+            expect(func1).to.have.a.nested.property('package.artifact').that.equals(expectedArtifactDestination),
+            expect(func2).to.have.a.nested.property('package.artifact').that.equals(expectedArtifactDestination)
           ])
         );
       });
@@ -586,12 +534,8 @@ describe('packageModules', () => {
           getVersionStub.returns(version);
           return expect(module.copyExistingArtifacts()).to.be.fulfilled.then(() =>
             BbPromise.all([
-              expect(func1)
-                .to.have.a.nested.property('package.artifact')
-                .that.equals(expectedArtifactPath),
-              expect(func2)
-                .to.have.a.nested.property('package.artifact')
-                .that.equals(expectedArtifactPath)
+              expect(func1).to.have.a.nested.property('package.artifact').that.equals(expectedArtifactPath),
+              expect(func2).to.have.a.nested.property('package.artifact').that.equals(expectedArtifactPath)
             ])
           );
         }).then(() =>
@@ -599,12 +543,8 @@ describe('packageModules', () => {
             getVersionStub.returns(version);
             return expect(module.copyExistingArtifacts()).to.be.fulfilled.then(() =>
               BbPromise.all([
-                expect(func1)
-                  .to.have.a.nested.property('artifact')
-                  .that.equals(expectedArtifactPath),
-                expect(func2)
-                  .to.have.a.nested.property('artifact')
-                  .that.equals(expectedArtifactPath),
+                expect(func1).to.have.a.nested.property('artifact').that.equals(expectedArtifactPath),
+                expect(func2).to.have.a.nested.property('artifact').that.equals(expectedArtifactPath),
                 expect(func1).to.have.a.nested.property('package.disable').that.is.true,
                 expect(func2).to.have.a.nested.property('package.disable').that.is.true
               ])
@@ -657,9 +597,7 @@ describe('packageModules', () => {
           const expectedArtifactPath = path.join('.serverless', 'test-service.zip');
 
           return expect(module.copyExistingArtifacts()).to.be.fulfilled.then(() =>
-            expect(serverless.service)
-              .to.have.a.nested.property('package.artifact')
-              .that.equals(expectedArtifactPath)
+            expect(serverless.service).to.have.a.nested.property('package.artifact').that.equals(expectedArtifactPath)
           );
         });
       });
@@ -694,12 +632,8 @@ describe('packageModules', () => {
             expect(fsMock.copyFileSync).to.be.calledWith(path.join('.webpack', 'func2.zip'), expectedFunc2Destination),
 
             // Should set package artifact locations
-            expect(func1)
-              .to.have.a.nested.property('package.artifact')
-              .that.equals(expectedFunc1Destination),
-            expect(func2)
-              .to.have.a.nested.property('package.artifact')
-              .that.equals(expectedFunc2Destination)
+            expect(func1).to.have.a.nested.property('package.artifact').that.equals(expectedFunc1Destination),
+            expect(func2).to.have.a.nested.property('package.artifact').that.equals(expectedFunc2Destination)
           ])
         );
       });
