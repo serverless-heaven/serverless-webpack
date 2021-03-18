ce5db2e2a540bc89f2a12aa47aa8d377766fae7f
Jeremy Benoist <jeremy.benoist@gmail.com>
2021-03-18 20:42:58 +0100

    Fix coverage drop

--- tests/mocks/bestzip.mock.js
+++ tests/mocks/bestzip.mock.js
@@ -9,8 +9,13 @@ const sinon = require('sinon');
 const BestZipMock = sandbox => sandbox.stub();
 
 module.exports.create = sandbox => {
+  const hasNativeZip = sinon.stub();
+  hasNativeZip.onCall(0).returns(false);
+  hasNativeZip.onCall(1).returns(true);
+  hasNativeZip.returns(false);
+
   return {
     bestzip: sinon.stub().resolves(BestZipMock(sandbox)),
-    hasNativeZip: sandbox.stub().returns(false),
+    hasNativeZip,
   };
 };
