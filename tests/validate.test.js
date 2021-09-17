3d313a230a2def5633a01842861d3d4a4765446f
Gabriel Debes <gabriel.wady@gmail.com>
2021-09-17 15:09:52 -0700

    Add test case and fixes usage of image param when simple remote string is used

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -795,6 +795,51 @@ describe('validate', () => {
         }).to.throw(/Either function.handler or function.image must be defined/);
       });
 
+      it('should not throw error if container image is a simple string', () => {
+        const testOutPath = 'test';
+        const testFunctionsConfig = {
+          func1: {
+            artifact: 'artifact-func1.zip',
+            events: [
+              {
+                http: {
+                  method: 'POST',
+                  path: 'func1path'
+                }
+              },
+              {
+                nonhttp: 'non-http'
+              }
+            ],
+            image: 'XXXX.dkr.ecr.ca-central-1.amazonaws.com/myproject/customNode:latest'
+          }
+        };
+
+        const testConfig = {
+          entry: 'test',
+          context: 'testcontext',
+          output: {
+            path: testOutPath
+          },
+          getFunction: func => {
+            return testFunctionsConfig[func];
+          }
+        };
+
+        _.set(module.serverless.service, 'custom.webpack.config', testConfig);
+        module.serverless.service.functions = testFunctionsConfig;
+        globSyncStub.callsFake(filename => [_.replace(filename, '*', 'js')]);
+        return expect(module.validate()).to.be.fulfilled.then(() => {
+          const lib = require('../lib/index');
+          const expectedLibEntries = {};
+
+          expect(lib.entries).to.deep.equal(expectedLibEntries);
+          expect(globSyncStub).to.have.callCount(0);
+          expect(serverless.cli.log).to.not.have.been.called;
+          return null;
+        });
+      });
+
       describe('google provider', () => {
         beforeEach(() => {
           _.set(module.serverless, 'service.provider.name', 'google');
