0672d43ab77e8a745610077c4241c7f2975f223c
Moroine <moroine.bentefrit@gmail.com>
2022-03-13 21:27:37 +0100

    fix: setup file

--- examples/include-external-npm-packages-lock-file/_setup.js
+++ examples/include-external-npm-packages-lock-file/_setup.js
@@ -1,35 +1,21 @@
 'use strict';
 
-const { spawn } = require('child_process');
-const fs = require('fs');
 const path = require('path');
 
-module.exports = originalFixturePath => {
+module.exports = async (originalFixturePath, fixturePath, utils) => {
   const pluginPath = path.resolve(originalFixturePath, '..', '..');
 
-  const SLS_CONFIG_PATH = path.join(__dirname, 'serverless.yml');
-  const slsConfig = fs.readFileSync(SLS_CONFIG_PATH);
-  // eslint-disable-next-line lodash/prefer-lodash-method
-  const updatedSlsConfig = String(slsConfig).replace('- serverless-webpack', `- ${pluginPath}`);
-  fs.writeFileSync(SLS_CONFIG_PATH, updatedSlsConfig);
+  const SLS_CONFIG_PATH = path.join(fixturePath, 'serverless.yml');
+  const WEBPACK_CONFIG_PATH = path.join(fixturePath, 'webpack.config.js');
+  const PACKAGE_JSON_PATH = path.join(fixturePath, 'package.json');
+  const LOCK_PATH = path.join(fixturePath, 'package-lock.json');
 
-  const WEBPACK_CONFIG_PATH = path.join(__dirname, 'webpack.config.js');
-  const webpackConfig = fs.readFileSync(WEBPACK_CONFIG_PATH);
-  // eslint-disable-next-line lodash/prefer-lodash-method
-  const updatedWebpackConfig = String(webpackConfig).replace("'serverless-webpack'", `'${pluginPath}'`);
-  fs.writeFileSync(WEBPACK_CONFIG_PATH, updatedWebpackConfig);
-  return new Promise((resolve, reject) => {
-    console.log(pluginPath);
-    spawn('npm', ['install', `serverless-webpack@file:${pluginPath}`, '--save-dev'], { cwd: __dirname })
-      .on('error', err => {
-        reject(err);
-      })
-      .on('close', exitCode => {
-        if (exitCode !== 0) {
-          reject(`Error: _setup failed with code ${exitCode}`);
-        } else {
-          resolve();
-        }
-      });
-  });
+  await Promise.all([
+    utils.replaceInFile(SLS_CONFIG_PATH, '- serverless-webpack', `- ${pluginPath}`),
+    utils.replaceInFile(WEBPACK_CONFIG_PATH, "'serverless-webpack'", `'${pluginPath}'`),
+    utils.replaceInFile(PACKAGE_JSON_PATH, 'file:../..', `file:${pluginPath}`),
+    utils.replaceInFile(LOCK_PATH, '../..', `${pluginPath}`)
+  ]);
+
+  return utils.spawnProcess('yarn', ['install'], { cwd: __dirname });
 };
