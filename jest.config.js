c03b044232ec4ccec5741d6e0b8d64157b7f55ed
Jeremy Benoist <jeremy.benoist@gmail.com>
2022-03-29 10:17:04 +0200

    Update Jest config
    - `verbose` by default to see the test suite progres
    - add `modulePathIgnorePatterns` to avoid error `Haste module naming collision`
    - Force color to have a nice output in GitHub Actions

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/tests/', '<rootDir>/coverage/', '<rootDir>/examples/'],
   coverageProvider: 'v8',
-  coverageReporters: ['lcov', 'clover', 'text-summary']
+  coverageReporters: ['lcov', 'clover', 'text-summary'],
+  modulePathIgnorePatterns: ['<rootDir>/examples/']
 };
