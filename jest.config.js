d6d21559156231fa1cd0718302d9f6b0018be58c
Vicary <vicary.archangel@member.mensa.org>
2022-12-29 02:59:11 +0800

    fix: test errors in node16

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/tests/', '<rootDir>/coverage/', '<rootDir>/examples/'],
   coverageProvider: 'v8',
   coverageReporters: ['lcov', 'clover', 'text-summary'],
-  modulePathIgnorePatterns: ['<rootDir>/examples/']
+  modulePathIgnorePatterns: ['<rootDir>/examples/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!serverless-offline/)']
 };
